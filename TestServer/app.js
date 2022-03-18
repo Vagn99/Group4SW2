de104ffe77eb0b6ceeaeb0356de29b0339ce4261
Vagn99 <christoffer.lorenzen@outlook.com>
2022-03-18 11:08:18 +0100

    Det virker!

--- TestServer/app.js
+++ TestServer/app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var numberRouter = require('./routes/number');
 
 var app = express();
 
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/number', numberRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
