8d361dff9697e648a8098450e1a32ec5558e9dbd
Vagn99 <christoffer.lorenzen@outlook.com>
2022-03-18 13:06:02 +0100

    Various updates

--- TestServer/routes/login.js
+++ TestServer/routes/login.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var router = express.Router();
+let names = ["admin", "user1", "user2", "user3", "user4", "user5", "user6"];
+let passwords = ["admin", "user1", "user2", "user3", "user4", "user5", "user6"];
 
 router.post('/', function(req, res) {
   const { name, password } = req.body;
   req.session = req.body;
   console.log(req.session);
-
-  if (name === "admin" && password === "admin") {
-    res.render("success", {
-      username: name,
-    });
+  for(i in names){
+    if(names[i] === name){
+      if(passwords[i] === password){
+        res.render("success", {
+          username: name,
+        });
+      }
+    }
   }
-  else{
-    res.send('Login failed');
-  }
+  res.send('Login failed');
 });
 
 module.exports = router;
