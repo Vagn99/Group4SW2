294aef03b513f8f0cd9d3582fb2c39a676a82184
Vagn99 <christoffer.lorenzen@outlook.com>
2022-03-18 13:36:10 +0100

    Login map

--- TestServer/routes/login.js
+++ TestServer/routes/login.js
@@ -1,22 +1,27 @@
 var express = require('express');
 var router = express.Router();
-let names = ["admin", "user1", "user2", "user3", "user4", "user5", "user6"];
-let passwords = ["admin", "user1", "user2", "user3", "user4", "user5", "user6"];
+
+let users = new Map();
+users.set("admin", "admin");
+users.set("user1", "user1");
+users.set("user2", "user2");
+users.set("user3", "user3");
+users.set("user4", "user4");
+users.set("user5", "user5");
+users.set("user6", "user6");
 
 router.post('/', function(req, res) {
   const { name, password } = req.body;
   req.session = req.body;
   console.log(req.session);
-  for(i in names){
-    if(names[i] === name){
-      if(passwords[i] === password){
-        res.render("success", {
-          username: name,
-        });
-      }
-    }
+  if(users.get(name) === password){
+    res.render("success", {
+      username: name,
+    });
   }
-  res.send('Login failed');
+  else{
+    res.send('Login failed');
+  }
 });
 
 module.exports = router;
