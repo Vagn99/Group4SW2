4742b6d4b02b691cedd3152b070c3ee0c78d91f3
PrometheusBork <tobiasmerithz@gmail.com>
2022-03-25 14:16:48 +0100

    map test

--- TestServer/app.js
+++ TestServer/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var numberRouter = require('./routes/number');
+var mapRouter = require('./routes/map');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/number', numberRouter);
+app.use('/map', mapRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
