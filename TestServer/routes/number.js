0bffc8114bb40b6f53f993af64ceb2a9001fc847
Profft <stefanlarsen@live.dk>
2022-03-25 11:18:16 +0100

    Multiple players with individual state

--- TestServer/routes/number.js
+++ TestServer/routes/number.js
@@ -9,17 +9,17 @@ let player1 = new Player.Player(1, "Beian", "Brian's Town", 1, 1);
 let players = new Map();
 players
     .set("user1", user1 = new Player.Player(1, "Beian", "Brian's Town", 1, 1))
-    .set("user2", "user2")
-    .set("user3", "user3")
-    .set("user4", "user4")
-    .set("user5", "user5")
-    .set("user6", "user6");
+    .set("user2", user2 = new Player.Player(1, "Beian", "Brian's Town", 1, 1))
+    .set("user3", user3 = new Player.Player(1, "Beian", "Brian's Town", 1, 1))
+    .set("user4", user4 = new Player.Player(1, "Beian", "Brian's Town", 1, 1))
+    .set("user5", user5 = new Player.Player(1, "Beian", "Brian's Town", 1, 1))
+    .set("user6", user6 = new Player.Player(1, "Beian", "Brian's Town", 1, 1));
 
 
 router.get('/get', function(req, res) {
-    console.log(req.session);
+    console.log(req.session.name);
 
-    res.send(player1.town.troopsInside + " troops in town");
+    res.send(players.get(req.session.name).town.troopsInside + " troops in town");
 
 
     //old code
@@ -27,10 +27,10 @@ router.get('/get', function(req, res) {
 });
 
 router.get('/set', function(req, res) {
-    let action = player1.town.barracks.trainTroops(player1.resources, player1.town.troopsInside);
+    let action = players.get(req.session.name).town.barracks.trainTroops(players.get(req.session.name).resources, players.get(req.session.name).town.troopsInside);
 
-    player1.resources = action.resources;
-    player1.town.troopsInside = action.troops;
+    players.get(req.session.name).resources = action.resources;
+    players.get(req.session.name).town.troopsInside = action.troops;
     res.send(action.message);
 
     //old code
