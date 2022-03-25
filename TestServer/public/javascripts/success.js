476d516ac38f84770383d2a6c9e34e93b6bb9173
Profft <stefanlarsen@live.dk>
2022-03-25 13:25:59 +0100

    Now you can only train 1 troop at a time

--- TestServer/public/javascripts/success.js
+++ TestServer/public/javascripts/success.js
@@ -1,3 +1,19 @@
+//Add event listener to button
+let button = document.getElementById("button");
+button.addEventListener("click", setCount);
+
+let countDown = document.getElementById("countDown");
+let working = false;
+
+
+//Runs at page load
+getCount();
+
+//Make the number update every X seconds
+setInterval(getCount, 5000);
+
+
+
 function getCount() {
     fetch('/number/get').then(response => {
         if (!response.ok) {
@@ -12,44 +28,37 @@ function getCount() {
     });
 }
 
-//Runs at page load
-getCount();
-
-//Make the number update every X seconds
-setInterval(getCount, 5000);
-
-//Add event listener to button
-let button = document.getElementById("button");
-button.addEventListener("click", setCount);
-
-let countDown = document.getElementById("countDown");
 
 //Request server to count
 function setCount() {
-    fetch('/number/set'
-    ).then(response => {
-        if (!response.ok) {
-            throw new Error("Response error: " + response.status)
-        }
-        return response.text();
-    }).then(text => {
-        let j = Number(text);
-        console.log(j);
-        countDown.textContent = "Troop ready in " + j + " seconds";
-        let intervalCount = setInterval(() => {
-            j--;
-            countDown.textContent = "Troop ready in " + j + " seconds";
-            if (j === 0) {
-                clearInterval(intervalCount);
-                countDown.textContent = "";
-                getCount();
+    if (!working) {
+        working = true;
+        fetch('/number/set'
+        ).then(response => {
+            if (!response.ok) {
+                throw new Error("Response error: " + response.status)
             }
+            return response.text();
+        }).then(text => {
+            let j = Number(text);
             console.log(j);
-        }, 1000)
-        console.log(text);
+            countDown.textContent = "Troop ready in " + j + " seconds";
+            let intervalCount = setInterval(() => {
+                j--;
+                countDown.textContent = "Troop ready in " + j + " seconds";
+                if (j === 0) {
+                    clearInterval(intervalCount);
+                    countDown.textContent = "";
+                    working = false;
+                    getCount();
+                }
+                console.log(j);
+            }, 1000)
+            console.log(text);
 
-    }).catch(error => {
-        console.log(error);
-    });
-    getCount();
+        }).catch(error => {
+            console.log(error);
+        });
+        getCount();
+    }
 }
