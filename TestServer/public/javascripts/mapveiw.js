31f1ca576dcc448b57ae4697377cf16bcb850d3e
Mikkel Christensen <mmc.99@hotmail.com>
2022-04-14 23:55:18 +0200

    id til click
    click til id

--- TestServer/public/javascripts/mapveiw.js
+++ TestServer/public/javascripts/mapveiw.js
@@ -5,5 +5,12 @@ function Test(id) {
 
 function show(btn) {
     console.log('Button id:',btn.id);
-    document.getElementById("dev1").textContent = 'Hex id:\n' + btn.id;
-  }
+    document.getElementById("dev1").textContent = "Hex id:\n"+ btn.id;
+}
+
+window.onload = function() {
+    document.addEventListener('click', function handleClick(event) {
+        console.log('Button id:',event.target.id);
+        document.getElementById("dev1").textContent = "Hex id:\n"+ event.target.id;
+    });
+}
