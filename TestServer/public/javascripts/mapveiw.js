b813d242bdb300e2bb2604f7ee6197330575a184
Mikkel Christensen <mmc.99@hotmail.com>
2022-04-15 19:17:23 +0200

    t
    tt

--- TestServer/public/javascripts/mapveiw.js
+++ TestServer/public/javascripts/mapveiw.js
@@ -1,16 +1,10 @@
-function Test(id) {
-    var id = id
-    console.log(id)
-}
-
-function show(btn) {
-    console.log('Button id:',btn.id);
-    document.getElementById("dev1").textContent = "Hex id:\n"+ btn.id;
-}
-
 window.onload = function() {
     document.addEventListener('click', function handleClick(event) {
         console.log('Button id:',event.target.id);
         document.getElementById("dev1").textContent = ""+ event.target.id;
     });
+}
+
+function start() {
+    fetch
 }
