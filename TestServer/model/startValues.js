a39b3fbe473ebbe4980e4983aa6b3d9949673395
Vagn99 <christoffer.lorenzen@outlook.com>
2022-05-13 13:49:50 +0200

    Update startValues.js

--- TestServer/model/startValues.js
+++ TestServer/model/startValues.js
@@ -1,18 +1,19 @@
 
 module.exports = {
-        startCommonIncome: 1,
-        startCommon: 20,
-        startGold: 0,
-        startCommonTileRecoursesPerSec: 1,
-        startGoldTileRecoursesPerSec: 1,
-        startTroopsNeutralTiles: 5,
+        startCommonIncome: 1, // 1
+        startCommon: 20, // 20
+        startGold: 0, // 0
+        startCommonTileRecoursesPerSec: 1, // 1
+        startGoldTileRecoursesPerSec: 1, // 1
+        startTroopsNeutralTiles: 5, // 5
 
-        startTroopsPlayers: 5,
-        startTroopsCost: 20,
-        startTroopTrainingTime: 3,
+        startTroopsPlayers: 5, // 5
+        startTroopsCost: 20, // 20
+        startTroopTrainingTime: 3, // 20
 
-        startTownHallUpgradeCost: [1,1],
-        townHallCostArray: [[1,1],[2,2],[3,3],[10,10],[40,20],[50,30],[60,70],[80,75],[100,150],[400,100],[500,300]],
-        startTownHallUpgradeTime: 10,
+        startTownHallUpgradeCost: [25,1],
+        townHallCostArray: [[50,25],[100,30],[120,40],[140,10],[40,20],[50,30],[60,70],[80,75],[100,150],[400,100]],
+        // townHallCostArray: [[1,1],[2,2],[3,3],[10,10],[40,20],[50,30],[60,70],[80,75],[100,150],[400,100],[500,300]],
+        startTownHallUpgradeTime: 1, // 10
 }
 
