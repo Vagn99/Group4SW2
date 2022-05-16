e17baabb8d120b33f30e46911268696e67b5d4d8
Vagn99 <christoffer.lorenzen@outlook.com>
2022-05-16 11:00:07 +0200

    Update startValues.js

--- TestServer/model/startValues.js
+++ TestServer/model/startValues.js
@@ -9,7 +9,7 @@ module.exports = {
 
         startTroopsPlayers: 5, // 5
         startTroopsCost: 20, // 20
-        startTroopTrainingTime: 3, // 20
+        startTroopTrainingTime: 3, // 3
 
         startTownHallUpgradeCost: [25,1],
         townHallCostArray: [[50,25],[100,30],[120,40],[140,40],[200,50],[600,50],[1000,70],[1200,75],[1500,100],[1500,100]],
